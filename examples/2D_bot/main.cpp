23e7890161b0afe5ee4647235fdfeab466542f02
Jay <jaykamat99@gmail.com>
2021-08-23 17:07:45 +0530

    New example added

--- examples/2D_bot/main.cpp
+++ examples/2D_bot/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <ompl/base/SpaceInformation.h>
-#include <ompl/base/spaces/RealVectorStateSpace.h>
-#include <ompl/base/spaces/special/TorusStateSpace.h>
-#include <ompl/geometric/SimpleSetup.h>
-#include <ompl/geometric/planners/prm/SPARS.h>
+#include <ompl/base/ProblemDefinition.h>
+#include <ompl/base/spaces/SE2StateSpace.h>
+#include <ompl/multilevel/planners/multimodal/LocalMinimaSpanners.h>
 
 #include <ompl/config.h>
 
@@ -12,49 +11,151 @@
 
 namespace ob = ompl::base;
 namespace og = ompl::geometric;
+namespace om = ompl::multilevel;
 
 struct ValidityCheckWithKOMO {
-  KOMO::Conv_KOMO_SparseNonfactored &nlp;
-  ValidityCheckWithKOMO(KOMO::Conv_KOMO_SparseNonfactored &nlp) : nlp(nlp) {}
-  bool check(const ob::State *state) {
+	KOMO::Conv_KOMO_SparseNonfactored &nlp;
+	ValidityCheckWithKOMO(KOMO::Conv_KOMO_SparseNonfactored &nlp) : nlp(nlp)
+	{
+	}
+	bool check(const ob::State *state)
+	{
+		// rai: uses quaternion with real part first
+		// ompl: uses quaternion with real part last
+		const auto *State = state->as<ob::SE2StateSpace::StateType>();
 
-    // rai: uses quaternion with real part first
-    // ompl: uses quaternion with real part last
-    const auto *TorusState = state->as<ob::TorusStateSpace::StateType>();
+		arr x_query =
+			arr{ State->getX(), State->getY(), State->getYaw() };
 
-    arr x_query = arr
-      {
-        TorusState->getS1(),
-        TorusState->getS2()
-      };
+		arr phi;
+		nlp.evaluate(phi, NoArr, x_query);
+		double tol = 1e-2;
 
-    arr phi;
-    nlp.evaluate(phi, NoArr, x_query);
-    double tol = 1e-2;
-
-    return std::abs(phi(0)) < tol;
-  }
+		return std::abs(phi(0)) < tol;
+	}
 };
 
-void visualize_random() {
-  auto filename = "2D_bot.g";
-  rai::Configuration C;
-  std::cout << C.getJointState() << std::endl;
-  C.addFile(filename);
-  C.watch(true);
+void plan()
+{
+	// construct the state space we are planning in
+	auto space(std::make_shared<ob::SE2StateSpace>());
 
-  // random configurations
-  size_t N = 20;
-  for (size_t i = 0; i < N; i++) {
-    arr pose = rand(3);
-    std::cout << pose << std::endl;
-    C.setJointState(pose);
-    C.watch(true);
-  }
+	// set the bounds
+	ob::RealVectorBounds bounds(2);
+	bounds.setLow(-2);
+	bounds.setHigh(2);
+	space->setBounds(bounds);
+
+	// set state validity checking based on KOMO
+	auto filename = "2D_bot.g";
+	rai::Configuration C;
+	C.addFile(filename);
+	KOMO komo;
+	komo.setModel(C, true);
+	komo.setTiming(1, 1, 1, 1);
+	komo.addObjective({}, FS_accumulatedCollisions, {}, OT_eq, { 1 });
+	komo.run_prepare(0);
+
+	// create instance of space information
+    auto si(std::make_shared<ob::SpaceInformation>(space));
+
+    // set state validity checking for this space
+	auto nlp = std::make_shared<KOMO::Conv_KOMO_SparseNonfactored>(komo, false);
+	ValidityCheckWithKOMO checker(*nlp);
+
+	si->setStateValidityChecker([&checker](const ob::State *state) {
+		return checker.check(state);
+	});
+
+    // create a start state
+    ob::ScopedState<> start(space);
+	start = {0,0,0};
+	// start[0] = komo.getConfiguration_q(0).elem(0);
+	// start[1] = komo.getConfiguration_q(0).elem(1);
+	// start[2] = komo.getConfiguration_q(0).elem(2);
+
+	std::cout << start << std::endl;
+
+    // create a goal state
+    ob::ScopedState<> goal(space);
+    goal[0] = 1;
+    goal[1] = 1;
+	goal[2] = 0;
+
+    // create an instance of problem definition
+    auto pdef(std::make_shared<ob::ProblemDefinition>(si));
+
+    // Set the start and goal states
+    pdef->setStartAndGoalStates(start, goal);
+
+	// Define planner
+    std::cout << "\nUsing Local Minima Spanner:" << std::endl;
+    std::vector<ob::SpaceInformationPtr> siVec;
+    siVec.push_back(si);
+    auto planner = std::make_shared<om::LocalMinimaSpanners>(siVec);
+    planner->setProblemDefinition(pdef);
+    planner->setup();
+
+	// attempt to solve the problem within ten seconds of planning time
+    ob::PlannerStatus solved = planner->ob::Planner::solve(10.0);
+    if (solved)
+    {
+        std::cout << "Found solution:" << std::endl;
+        auto localMinimaTree = planner->getLocalMinimaTree();
+        int NumberOfMinima =  (int)localMinimaTree->getNumberOfMinima();
+        int NumberOfLevels =  (int)localMinimaTree->getNumberOfLevel();
+        // std::ofstream out;
+        // out.open("Paths_out.txt", std::ios_base::app);
+        // out << NumberOfMinima*NumberOfLevels << "\n";
+        for (int i=0; i<NumberOfLevels; i++){
+            for (int j=0; j<NumberOfMinima; j++){
+                std::cout << "\n New path[" << i << j << "] \n" << std::endl;
+                std::dynamic_pointer_cast<ompl::geometric::PathGeometric>(localMinimaTree->getPath(i,j)->asPathPtr())->print(std::cout);
+                // std::dynamic_pointer_cast<ompl::geometric::PathGeometric>(localMinimaTree->getPath(i,j)->asPathPtr())->printAsMatrix(out);
+            }
+        }
+    }
+    else
+        std::cout << "No solution found" << std::endl;
 }
 
-int main(int /*argc*/,char** /*argv*/){
-  visualize_random();
+void visualize_random()
+{
+	auto filename = "2D_bot.g";
+	rai::Configuration C;
+	C.addFile(filename);
+	KOMO komo;
+	komo.setModel(C, true);
+	komo.setTiming(1, 1, 1, 1);
+	komo.addObjective({}, FS_accumulatedCollisions, {}, OT_eq, { 1 });
+	komo.run_prepare(0);
 
-  return 0;
+	auto nlp = std::make_shared<KOMO::Conv_KOMO_SparseNonfactored>(komo,
+								       false);
+
+	ObjectiveTypeA ot;
+	nlp->getFeatureTypes(ot);
+	C.watch(true);
+
+	arr phi;
+	size_t N = 20;
+	for (size_t i = 0; i < N; i++) {
+		arr pose = rand(3);
+		C.setJointState(pose);
+		nlp->evaluate(phi, NoArr, pose);
+		std::cout << pose << " " << phi << std::endl;
+		std::cout << komo.getConfiguration_q(0) << std::endl;
+		C.watch(true);
+	}
+}
+
+int main(int /*argc*/, char ** /*argv*/)
+{
+	/// \brief visualize_random samples random orientations and also checks
+	/// for collissions.
+	// visualize_random();
+
+	plan();
+
+	return 0;
 }
